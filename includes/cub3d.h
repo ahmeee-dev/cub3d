8603c919af1355ce8ad4e8799b8fac78f6c4efea
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-05-06 16:59:37 +0200

    started the parsing part

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,12 +6,25 @@
 /*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:29:10 by apintaur          #+#    #+#             */
-/*   Updated: 2025/05/06 14:20:59 by ahabdelr         ###   ########.fr       */
+/*   Updated: 2025/05/06 16:19:02 by ahabdelr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 #define CUB3D_H
+
+#include "../libft/includes/libft.h"
+#include <unistd.h>
+
+typedef struct	s_colors
+{
+	int	floor;
+	int	ceiling;
+	char	*nt;
+	char	*st;
+	char	*et;
+	char	*wt;
+}		t_colors;
 
 
 #endif
