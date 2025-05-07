12eae77b12a13925acf56cad1663abd634a304bf
denuen <anto.pintauro1@gmail.com>
2025-05-07 11:46:39 +0200

    fix: fixed header file

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahabdelr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: apintaur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:29:10 by apintaur          #+#    #+#             */
-/*   Updated: 2025/05/07 11:12:48 by ahabdelr         ###   ########.fr       */
+/*   Updated: 2025/05/07 11:46:19 by apintaur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,62 @@ typedef struct	s_colors
 	char	*et;
 	char	*wt;
 }		t_colors;
+typedef struct s_color
+{
+	unsigned int	t;
+	unsigned int	r;
+	unsigned int	g;
+	unsigned int	b;
+}	t_color;
+
+typedef struct s_point
+{
+	float			x;
+	float			y;
+	float			z;
+	unsigned int	color;
+}	t_point;
+
+typedef struct s_window
+{
+	void			*p;
+	unsigned int	color;
+}	t_window;
+
+typedef struct s_image
+{
+	void			*p;
+	char			*addr;
+	int				bits_pp;
+	int				lenght;
+	int				endian;
+	unsigned int	color;
+}	t_image;
+
+
+typedef struct	s_picture
+{
+	t_window	win;
+	t_image		img;
+}	t_picture;
+
+typedef struct s_mlx
+{
+	void		*mlx_p;
+	t_picture	pic;
+}	t_mlx;
+
+
+//Rendering functions
+int		mymlx_exit(t_mlx *data);
+int		mymlx_render(t_mlx *data);
+int		mymlx_destroy(t_mlx *data);
+void	mymlx_init_spawn(t_mlx *data);
+void	mymlx_init(t_mlx *data, char *filename);
+void	mymlx_pixel_put(t_mlx *data, int x, int y, int color);
+
 
 
 void	get_graphics(char *file, t_colors *colors);
-
 
 #endif
